79ff51c648a01b372789121737d6e7700c7bbc0b
Daniel Tomes <daniel.tomes@cloudera.com>
2017-03-15 23:38:12 +0000

    completed vds metadata parquet conversion

---
+++ metadata.py
@@ -0,0 +1,44 @@
+import os
+from pyspark.sql import HiveContext
+#vds = vds_path
+#vds
+#database = 'hail_vds'
+#metadata_tablename = 'vds_metadata'
+
+def create_metadata_table(sc, database, metadata_tablename, vds, df):
+  hiveContext = HiveContext(sc)
+  print 'Hive database is: %s' % database
+  print 'Hive Table Name is: %s' % metadata_tablename
+  print 'Database Table Location is: %s/metadata.parquet' % vds
+  hiveContext.sql('create database if not exists ' + database + ' location \'' + vds + '/db\'')
+  df.write.saveAsTable(database + '.' + metadata_tablename, path=vds + '/metadata.parquet')
+  
+def build_parquet_metadata(vds_path, sc, spark, create_tables=False, database='default',\
+                           metadata_tablename='vds_metadata'):
+  vds = vds_path
+  print "Creating processing directory"
+  os.system('hadoop fs -mkdir ' + vds + '/processing')
+  print "Cloning metadata"
+  os.system('hadoop fs -cp ' + vds + '/metadata.json.gz ' + vds + '/processing')
+  rawJsonFile = sc.wholeTextFiles(vds + '/processing')
+  print "Converting Metadata to Parquet"
+  processedJsonFile = rawJsonFile.map(lambda line: line[1].replace('\n','').replace(' ', ''))
+  df = spark.read.json(processedJsonFile)
+  
+  if create_tables:
+    print "Creating Hive Metastore Table"
+    create_metadata_table(sc, database, metadata_tablename, vds_path, df)
+    
+  else:
+    df.coalesce(1).write.save(vds + '/processing/metadata.paruqet')
+    os.system('hadoop fs -mkdir ' + vds + '/metadata')
+    os.system('hadoop fs -cp ' + vds + '/processing/metadata.paruqet/part* ' + vds + '/metadata/metadata.snappy.parquet')  
+
+  os.system('hadoop fs -rm -r ' + vds + '/processing')
+  
+    
+#    
+#select sa.id, gs.gq, gs.dp, gs.gt, sa.annotation.pheno.caffeineconsumption, sa.annotation.pheno.isfemale, sa.annotation.pheno.purplehair
+#from 1kg_vds.gs gs
+#inner join 1kg_vds_metadata.sample_annotations sa on 
+#    sa.pos = gs.pos
---
+++ process.py
@@ -0,0 +1,52 @@
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import seaborn
+import metadata
+from math import log, isnan
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+spark = SparkSession\
+    .builder\
+    .config("spark.jars", "/home/sense/hail-all-spark.jar")\
+    .config("spark.submit.pyFiles", "/home/sense/hail-python.zip")\
+    .config("spark.hadoop.io.compression.codecs", "org.apache.hadoop.io.compress.DefaultCodec,is.hail.io.compress.BGzipCodec,org.apache.hadoop.io.compress.GzipCodec")\
+    .config("spark.sql.files.openCostInBytes", "1099511627776")\
+    .config("spark.sql.files.maxPartitionBytes", "1099511627776")\
+    .config("spark.hadoop.mapreduce.input.fileinputformat.split.minsize", "1099511627776")\
+    .config("spark.hadoop.parquet.block.size", "1099511627776")\
+    .appName("Hail")\
+    .getOrCreate()
+sc = spark.sparkContext
+sqlContext = SQLContext(sc)
+sc.addPyFile('metadata.py')
+import sys
+sys.path.append('/home/sense/hail-python.zip')
+
+from hail import *
+hc = HailContext(sc)
+
+#! wget https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz
+
+#! tar -xvzf Hail_Tutorial_Data-v2.tgz
+
+#! hadoop fs -put Hail_Tutorial-v2 .
+
+vds = hc.import_vcf('Hail_Tutorial-v2/1000Genomes_248samples_coreExome10K.vcf.bgz')
+
+vds = vds.split_multi()
+
+vds = vds.annotate_samples_table('Hail_Tutorial-v2/1000Genomes.sample_annotations',
+                                 root='sa.pheno', 
+                                 sample_expr='Sample', 
+                                 config=TextTableConfig(impute=True))
+
+! hadoop fs -cat Hail_Tutorial-v2/1000Genomes.sample_annotations | head -n 200
+
+out_path = '1kg.vds'
+vds.write(out_path, parquet_genotypes=True)
+
+vds_path = '/user/tomesd/1kg.vds'
+metadata.build_parquet_metadata(vds_path, sc, spark, create_tables=True, database='hail_vds_tomesd')
